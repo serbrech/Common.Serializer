dd7b23583f2796e0c352ccafd9b34e2924fa1400
David Lindblad <david.lindblad@hotmail.com>
2014-12-14 01:17:26 +0100

    Added Binary Serializer

---
+++ src/Common.Serializer/Binary/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Serializer.Binary
+{
+    public static class Extensions
+    {
+        public static ISerialize Binary(this ISerializationContext context)
+        {
+            return new BinarySerializerAdapter();
+        }
+    }
+}

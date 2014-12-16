2d6aa79023ccb9450dbba000d58636908bdb605e
unknown <per.svensson.se@gmail.com>
2014-12-16 04:15:51 +0100

    Added IEnum serialize and type deserializations tests for Json formatter

--- src/Common.Serializer.Tests/JsonTest.cs
+++ src/Common.Serializer.Tests/JsonTest.cs
@@ -1,12 +1,20 @@
 ﻿using Common.Serializer.NewtonsoftJson;
 using FluentAssertions;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Common.Serializer.Tests
 {
     [TestFixture]
     public class JsonTest
     {
+        string serializedString = "{\"IntProp\":1000,\"SomeString\":\"This is some nasty string... not really\"}";
+        SerializeMe deserializedType = new SerializeMe
+        {
+            IntProp = 1000,
+            SomeString = "This is some nasty string... not really"
+        };
+
         [SetUp]
         public void Setup()
         {
@@ -19,12 +27,23 @@ namespace Common.Serializer.Tests
         [Test]
         public void SerializeTest()
         {
-            Serialization.Serialize(new SerializeMe
-            {
-                IntProp = 1000,
-                SomeString = "This is some nasty string... not really"
-            })
-            .Should().Be("{\"IntProp\":1000,\"SomeString\":\"This is some nasty string... not really\"}");
+            Serialization.Serialize(deserializedType)
+            .Should().Be(serializedString);
+        }
+
+        [Test]
+        public void DeserializeTest()
+        {
+            var result = Serialization.Deserialize<SerializeMe>(serializedString);
+            Assert.AreEqual(deserializedType.IntProp, result.IntProp);
+        }
+
+        [Test]
+        public void SerializeTListTest()
+        {
+            IEnumerable<SerializeMe> list = new List<SerializeMe>{ deserializedType, deserializedType};
+            Serialization.Serialize(list)
+            .Should().Be(string.Format("[{0},{1}]", serializedString, serializedString));
         }
     }
 }
